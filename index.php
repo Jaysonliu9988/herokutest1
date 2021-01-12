31852519f28f09d827ccfab0d5cd1ab7b4579a10
Jayson Liu <52879802+Jaysonliu9988@users.noreply.github.com>
2021-01-12 22:31:05 +0800

    add index.php files and other files for test

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Books</title>
 
-echo "hello php";
+    <script src="https://kit.fontawesome.com/e41cb624b5.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-?>
+    <!-- Custom stylesheet-->
+    <link rel="stylesheet" href="style.css">    
+
+</head>
+<body>
+<h1 class="py-4 bg-dark text-light rounded"><i class="fas fa-swatchbook"></i> Book Store</h1>
+</body>
+</html>
