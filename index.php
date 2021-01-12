3531dcec43acef9ebada772428073aab9835b4b9
Jayson Liu <52879802+Jaysonliu9988@users.noreply.github.com>
2021-01-12 23:08:10 +0800

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // require_once realpath("../vendor/autoload.php");
 require_once ("src/php/component.php");
-require_once ("src/php/operation.php");
+// require_once ("src/php/operation.php");
 
 ?>
 
@@ -90,6 +90,6 @@ require_once ("src/php/operation.php");
 </main>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-<script src="src/php/main.js"></script>
+<!-- <script src="src/php/main.js"></script> -->
 </body>
 </html>
