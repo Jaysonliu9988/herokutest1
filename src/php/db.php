cf7285f9f95f41c2aeff2971d1e215a30259a6ac
Jayson Liu <52879802+Jaysonliu9988@users.noreply.github.com>
2021-01-16 22:20:30 +0800

    add database file

---
+++ src/php/db.php
@@ -0,0 +1,43 @@
+<?php
+
+function Createdb(){
+    $servername = "us-cdbr-east-03.cleardb.com";
+    $username = "b2e7185b924846";
+    $password = "7305f455";
+    $dbname = "bookstore";
+
+    // create connection
+    $con = mysqli_connect($servername, $username, $password);
+
+    // Check Connection
+    if (!$con){
+        die("Connection Failed : " . mysqli_connect_error());
+    }
+
+    // create Database
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+
+    if(mysqli_query($con, $sql)){
+        $con = mysqli_connect($servername, $username, $password, $dbname);
+
+        $sql = "
+                CREATE TABLE IF NOT EXISTS books(
+                    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    book_name VARCHAR (25) NOT NULL,
+                    book_publisher VARCHAR (20),
+                    book_price FLOAT 
+                );
+        ";
+
+        if(mysqli_query($con, $sql)){
+            return $con;
+        }else{
+            echo"cannot create table...!";
+        }
+
+    }else{
+        echo "Error while creating database ". mysqli_error($con);
+    }
+
+}
